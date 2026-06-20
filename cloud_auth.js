6f192dc4ac65ea450f0c725c50b00b5550a7b395
ssss69 <sarkersreenidhi@gmail.com>
2026-06-20 15:18:20 +0530

    Add files via upload

--- cloud_auth.js
+++ cloud_auth.js
@@ -1,5 +1,5 @@
 (function () {
-  const API = window.location.protocol === "file:" ? "http://127.0.0.1:8765" : window.location.origin;
+  const API = window.ANTIMONY_API || (window.location.protocol === "file:" ? "http://127.0.0.1:8765" : window.location.origin);
   window.ANTIMONY_API = API;
 
   const state = {
